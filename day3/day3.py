c1e9bf10479d689d77b2b225ef83342e52094ee9
Caleb Morales <clbmrls@gmail.com>
2020-12-03 10:26:52 -0700

    finished day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,3 +1,31 @@
+def findTrees(lines, right, down):
+    indent = 0
+    totalTrees = 0
+    skip = down
+    for line in lines:
+        if skip is not down:
+            skip += 1
+        else:
+            line = line.rstrip('\n')
+            if line[indent] == '#':
+                totalTrees += 1
+            indent += right
+            if indent >= len(line):
+                indent = indent - len(line)
+            skip = 1
+    return totalTrees
+
+def day3Part1(lines):
+    totalTrees = findTrees(lines, 3, 1)
+    return totalTrees
+
+def day3Part2(lines):
+    a = findTrees(lines, 1, 1)
+    b = findTrees(lines, 3, 1)
+    c = findTrees(lines, 5, 1)
+    d = findTrees(lines, 7, 1)
+    e = findTrees(lines, 1, 2)
+    return a*b*c*d*e
 
 
 
@@ -10,9 +38,9 @@ def main(inputFile):
 
     file.close()
 
-    # print("Part 1:", day3Part1(lines), "valid passwords")
-    # print("Part 2:", day3Part2(lines), "valid passwords")
+    print("Part 1:", day3Part1(lines), "total trees")
+    print("Part 2:", day3Part2(lines), "is the answer")
 
 print("Advent of Code: Day 3")
 print("Caleb Morales - clbmrls14")
-main("input/sampleInput.txt")
+main("input/input.txt")
